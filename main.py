17f02688b5099f9ebe1ed4504e8ac5d87b4200af
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2021-02-02 15:19:49 -0500

    Added try catch for bytes

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ class crypto:
     return repr(self.content)
 
   def mode_convert(self):
-    if type(self.content) == str:
-      self.content = bytes(self.content, "ascii")
-    else:
-      self.content = self.content.decode("ascii")
+    try:
+      if type(self.content) == str:
+        self.content = bytes(self.content, "ascii")
+      else:
+        self.content = self.content.decode("ascii")
+      return self
+    except:
+        pass
 
   def xor(self, key):
     if type(self.content) == str:
@@ -19,9 +23,13 @@ class crypto:
       self.xor(key)
       self.mode_convert()
     else:
-      key = bytes(key, "ascii")
-      key_repeated = key*(len(self.content)//len(key)) + key[:(len(self.content)%len(key))]
-      self.content = bytearray(i^j for i,j in zip(self.content,key_repeated))
+      try:
+          key = bytes(key, "ascii")
+          key_repeated = key*(len(self.content)//len(key)) + key[:(len(self.content)%len(key))]
+          self.content = bytearray(i^j for i,j in zip(self.content,key_repeated))
+          return self
+      except:
+          pass
 
   def xor_int(self, key):
     if type(self.content) == str:
@@ -30,12 +38,15 @@ class crypto:
       self.mode_convert()
     else:
       self.content = bytearray(i^key for i in self.content)
+    return self
 
-data = [crypto(base64.b64decode(open("input.txt", "r").read())).xor_int(i) for i in range(256)]
+text = base64.b64decode(open("input.txt", "r").read())
 
-[elem.mode_convert() for elem in data]
+data = {i:crypto(text) for i in range(128)}
 
-print('\n'.join([str(i)+": "+repr(elem)[:10] for elem,i in zip(data, range(len(data)))]))
+data = {k:v.xor_int(k).mode_convert() for k,v in data.items()}
+
+print('\n'.join([str(i)+": "+elem.content[:10] for i,elem in data.items()]))
 
 while True:
     print(repr(data[int(input("Select value for more data: "))]))
